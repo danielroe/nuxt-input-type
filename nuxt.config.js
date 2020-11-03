dcfc0320ad03244d00b61b8ed0a7c63d7f972cc9
Daniel Roe <daniel@parentscheme.com>
2020-11-03 09:39:14 +0000

    chore: init

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+export default {
+  target: "static",
+
+  head: {
+    title: "Nuxt.js starter for CSB",
+    htmlAttrs: {
+      lang: "en"
+    }
+  },
+
+  buildModules: ["@nuxtjs/html-validator"]
+};
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <form>
+    <label>
+      An input
+      <input type="text" />
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+</template>
